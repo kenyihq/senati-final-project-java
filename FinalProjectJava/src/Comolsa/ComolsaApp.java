f7b534c53575a14e85a844cc25aeb51452002c99
Kenyi Hancco <kenyihq@gmail.com>
2021-06-20 02:19:48 -0500

    Se termino el proyecto, faltan arregalar algunos bugs

---
+++ FinalProjectJava/src/Comolsa/ComolsaApp.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Comolsa;
+
+/**
+ *
+ * @author kenyi
+ */
+public class ComolsaApp {
+    
+    public static void main(String[] args) {
+        
+        Index I = new Index();
+    }
+}
